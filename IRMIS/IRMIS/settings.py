9900f9195093d79092bc004d2aac6ab336e23014
Sérgio Esteves <sergioesteves7@gmail.com>
2019-07-30 11:31:49 +0900

    blackened code

--- IRMIS/IRMIS/settings.py
+++ IRMIS/IRMIS/settings.py
@@ -162,9 +162,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'dist'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "dist"),)
 
 # Media files (user uploaded files)
 
