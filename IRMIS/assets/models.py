878a7345c39b768b363d4ab39799173b469a5cd0
Kevin Keller <flygeneticist@gmail.com>
2019-08-06 14:11:31 +0800

    more fixes to model & migrations

--- IRMIS/assets/models.py
+++ IRMIS/assets/models.py
@@ -1,28 +1,23 @@
 from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
+import django.db.models.deletion
 
 
 class Shapefile(models.Model):
     filename = models.CharField(max_length=255)
-    file = models.FileField(upload_to="media/shapefiles")
+    file = models.FileField(upload_to="media/shapefiles", null=True)
     srs = models.CharField(max_length=255)
     file_update_date = models.DateField(_(""), auto_now=False, auto_now_add=False)
 
 
-class Feature(models.Model):
-    shapefile = models.ForeignKey(Shapefile, on_delete="")
+class Road(models.Model):
+    shapefile = models.ForeignKey(Shapefile, null=True, on_delete=django.db.models.deletion.CASCADE)
     name = models.CharField(
         verbose_name=_("Name"),
         null=False,
         blank=False,
         max_length=150,
     )
-
-    def __str__(self):
-        return self.name
-
-
-class Road(Feature):
     geometry = models.MultiLineStringField(
         verbose_name=_("Name"),
         help_text=_("The path of the road"),
@@ -31,8 +26,18 @@ class Road(Feature):
         null=True
     )
 
+    def __str__(self):
+        return self.name
 
-class Bridge(Feature):
+
+class Bridge(models.Model):
+    shapefile = models.ForeignKey(Shapefile, on_delete="")
+    name = models.CharField(
+        verbose_name=_("Name"),
+        null=False,
+        blank=False,
+        max_length=150,
+    )
     geometry = models.MultiPolygonField(
         verbose_name=_("Name"),
         help_text=_("The area of the bridge"),
@@ -40,3 +45,6 @@ class Bridge(Feature):
         blank=True,
         null=True
     )
+
+    def __str__(self):
+        return self.name
