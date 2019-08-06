7f3f5ceda8704e8e5ba4e7035d834c1aaca81d90
Kevin Keller <flygeneticist@gmail.com>
2019-08-06 12:56:10 +0800

    new model to hold shapefile ref data + road/bridge

--- IRMIS/assets/models.py
+++ IRMIS/assets/models.py
@@ -2,20 +2,43 @@ from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class Road(models.Model):
+class Shapefile(models.Model):
+    filename = models.CharField(max_length=255)
+    file = models.FileField(upload_to="/media")
+    srs = models.CharField(max_length=255)
+    encoding = models.CharField(max_length=20)
+    meta_data = models.JSONField(encoder="")
+    file_update_date = models.DateField(_(""), auto_now=False, auto_now_add=False)
+
+
+class Feature(models.Model):
+    shapefile = models.ForeignKey(Shapefile)
     name = models.CharField(
         verbose_name=_("Name"),
-        help_text=_("Add a name to help identity this road"),
         null=False,
         blank=False,
-        max_length=50,
-    )
-    geometry = models.MultiLineStringField(
-        verbose_name=_("Name"),
-        help_text=_("The path of the road"),
-        null=True,
-        blank=True,
+        max_length=150,
     )
 
     def __str__(self):
         return self.name
+
+
+class Road(Feature):
+    geometry = models.MultiLineStringField(
+        verbose_name=_("Name"),
+        help_text=_("The path of the road"),
+        srid=4326,
+        blank=True,
+        null=True
+    )
+
+
+class Bridge(Feature):
+    geometry = models.MultiPolygonField(
+        verbose_name=_("Name"),
+        help_text=_("The area of the bridge"),
+        srid=4326,
+        blank=True,
+        null=True
+    )
