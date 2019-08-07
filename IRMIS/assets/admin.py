68074a71d1ee67fad201a0a7959b75baba0ac4e0
Kevin Keller <flygeneticist@gmail.com>
2019-08-07 14:05:28 +0800

    add new models & remove old ones

--- IRMIS/assets/admin.py
+++ IRMIS/assets/admin.py
@@ -1,4 +1,6 @@
 from django.contrib.gis import admin
-from .models import Road
+from .models import RoadNat, RoadMuni, RoadRural
 
-admin.site.register(Road, admin.OSMGeoAdmin)
+admin.site.register(RoadNat, admin.OSMGeoAdmin)
+admin.site.register(RoadMuni, admin.OSMGeoAdmin)
+admin.site.register(RoadRural, admin.OSMGeoAdmin)
--- IRMIS/assets/models.py
+++ IRMIS/assets/models.py
@@ -1,50 +1,5 @@
-from django.contrib.gis.db import models
-from django.utils.translation import ugettext_lazy as _
-import django.db.models.deletion
-
-
-class Shapefile(models.Model):
-    filename = models.CharField(max_length=255)
-    file = models.FileField(upload_to="media/shapefiles", null=True)
-    feature_type = models.CharField(max_length=50, null=True)
-    file_update_date = models.DateField(_(""), auto_now=False, auto_now_add=False)
-
-
-class Road(models.Model):
-    shapefile = models.ForeignKey(Shapefile, null=True, on_delete=django.db.models.deletion.CASCADE)
-    name = models.CharField(
-        verbose_name=_("Name"),
-        null=False,
-        blank=False,
-        max_length=50,
-    )
-    geometry = models.MultiLineStringField(
-        verbose_name=_("Name"),
-        help_text=_("The path of the road"),
-        srid=4326,
-        blank=True,
-        null=True
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class Bridge(models.Model):
-    shapefile = models.ForeignKey(Shapefile, null=True, on_delete=django.db.models.deletion.CASCADE)
-    name = models.CharField(
-        verbose_name=_("Name"),
-        null=False,
-        blank=False,
-        max_length=50,
-    )
-    geometry = models.MultiPolygonField(
-        verbose_name=_("Name"),
-        help_text=_("The area of the bridge"),
-        srid=4326,
-        blank=True,
-        null=True
-    )
-
-    def __str__(self):
-        return self.name
+# import unmanaged feature models from shapefiles
+try:
+    from .feature_models import *
+except ImportError:
+    pass
