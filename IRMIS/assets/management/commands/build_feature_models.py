0f2f4641efcde8fc44c11eb2866331b23a322264
Kevin Keller <flygeneticist@gmail.com>
2019-08-08 12:05:52 +0800

    improve management commands

--- IRMIS/assets/management/commands/build_feature_models.py
+++ IRMIS/assets/management/commands/build_feature_models.py
@@ -3,10 +3,10 @@ from assets.tasks.shapefiles import create_unmanged_models
 
 
 class Command(BaseCommand):
-    help = 'Import shapefile GIS data into postgresql table'
+    help = 'Build unmanaged Django models from shapefiles derived Postgresql tables'
 
     def add_arguments(self, parser):
-        parser.add_argument('--dryrun', default=False, help='Whether or not to save models')
+        parser.add_argument('-p', default=False, type=bool, help='Prints output, but does not save')
 
     def handle(self, *args, **options):
         create_unmanged_models(dryrun=options.get('dryrun'))
--- IRMIS/assets/management/commands/import_shapefile_features.py
+++ IRMIS/assets/management/commands/import_shapefile_features.py
@@ -1,20 +1,21 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from assets.tasks.shapefiles import import_shapefile_features
 
 
 class Command(BaseCommand):
-    help = 'Build unmanaged Django models from shapefiles derived Postgresql tables'
+    help = 'Import shapefile GIS data into PostgreSQL table'
 
     def add_arguments(self, parser):
-        parser.add_argument('--filename', default=None, help='The shapefile to import into PostgreSQL')
-        parser.add_argument('--feature', default=None, help='The type of feature in the shapefile')
+        parser.add_argument('--filename', required=True, help='The shapefile to import into PostgreSQL')
+        parser.add_argument('--table', required=True, choices=getattr(settings, 'FEATURE_TABLES', []), help='The table name of the feature to import')
         parser.add_argument('--meta', default=None, help='Meta data to add to the table (as JSON string)')
-        parser.add_argument('--dryrun', default=False, help='Whether or not to save models')
+        parser.add_argument('-p', default=False, type=bool, help='Prints output, but does not save')
 
     def handle(self, *args, **options):
         import_shapefile_features(
             filename=options.get('filename'),
-            feature=options.get('feature'),
+            table=options.get('table'),
             meta=options.get('meta'),
-            dryrun=options.get('dryrun')
+            dryrun=options.get('p')
         )
