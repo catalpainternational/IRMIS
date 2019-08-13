ff2f57f0aab745c925b598c3246cb4d9c65b0f65
Kevin Keller <flygeneticist@gmail.com>
2019-08-13 10:11:39 +0800

    adding better error checking to shapefile task functions

--- IRMIS/assets/tasks/shapefiles.py
+++ IRMIS/assets/tasks/shapefiles.py
@@ -17,39 +17,66 @@ file_path = os.path.join(os.path.abspath(__file__ + "/../../"), "feature_models"
 def import_shapefile_features(filename, table, meta=None, dryrun=False):
     if table in valid_features:
         try:
+            # check if SQL table exists to determine shp2pgsql command to use
+            check_sql = "select exists(select count(*) from %s)" % table
+            c = connection.cursor()
+            c.execute(check_sql)
+            if c.fetchone()[0]:
+                cmd = "shp2pgsql -I -d -s 32751 %s %s" % (filename, table)
+            else:
+                cmd = "shp2pgsql -I -c -s 32751 %s %s" % (filename, table)
+
             if not dryrun:
                 try:
                     # write SQL2PGSQL output to temp file
-                    cmd = "shp2pgsql -I -c -s 32751 %s %s" % (filename, table)
+                    err = None
                     with open("feature_sql_temp.sql", "w") as sql_f:
-                        subprocess.call(cmd, shell=True, stdout=sql_f)
-                    # read temp SQL file commands into cursor for execution
-                    with open("feature_sql_temp.sql", "r") as f:
-                        with connection.cursor() as c:
-                            file_data = f.readlines()
-                            statement = ""
-                            delimiter = ";\n"
-                            for line in file_data:
-                                if re.findall("DELIMITER", line):  # found delimiter
-                                    if re.findall("^\s*DELIMITER\s+(\S+)\s*$", line):
-                                        delimiter = (
-                                            re.findall(
+                        sql_process = subprocess.run(
+                            cmd, shell=True, stdout=sql_f, stderr=err
+                        )
+                    try:
+                        assert err is None
+                        print(
+                            "SQL generation from Shapefile was successful. Reading SQL to DB..."
+                        )
+                        # read temp SQL file commands into cursor for execution
+                        with open("feature_sql_temp.sql", "r") as f:
+                            with connection.cursor() as c:
+                                file_data = f.readlines()
+                                statement = ""
+                                delimiter = ";\n"
+                                try:
+                                    for line in file_data:
+                                        if re.findall(
+                                            "DELIMITER", line
+                                        ):  # found delimiter
+                                            if re.findall(
                                                 "^\s*DELIMITER\s+(\S+)\s*$", line
-                                            )[0]
-                                            + "\n"
-                                        )
-                                        continue
-                                    else:
-                                        raise SyntaxError(
-                                            "Your use of DELIMITER is not correct"
-                                        )
-                                # add lines while not met lines with current delimiter
-                                statement += line
-                                if line.endswith(delimiter):
-                                    # execute current statement
-                                    c.execute(statement)
-                                    # begin collect next statement
-                                    statement = ""
+                                            ):
+                                                delimiter = (
+                                                    re.findall(
+                                                        "^\s*DELIMITER\s+(\S+)\s*$",
+                                                        line,
+                                                    )[0]
+                                                    + "\n"
+                                                )
+                                                continue
+                                            else:
+                                                raise SyntaxError(
+                                                    "Your use of DELIMITER is not correct"
+                                                )
+                                        # add lines while not met lines with current delimiter
+                                        statement += line
+                                        if line.endswith(delimiter):
+                                            # execute current statement
+                                            c.execute(statement)
+                                            # begin collect next statement
+                                            statement = ""
+                                except Exception as e:
+                                    print("ERROR reading SQL into DB - %s" % e)
+                        print("Successfully read SQL to the DB.")
+                    except AssertionError as e:
+                        print("SQL generation from Shapefile was NOT successful.")
                     os.remove("feature_sql_temp.sql")
                 except Exception:
                     # last minute clean up check of temp SQL file
@@ -78,25 +105,24 @@ def create_unmanged_model(table, dryrun=False):
             model_file = os.path.join(file_path, "%s.py" % table)
             if os.path.isfile(model_file):
                 os.rename(model_file, model_file + ".old")
+
+            err = None
             with open(model_file, "w") as f:
                 # write output to file
-                call_command("inspectdb", table, stdout=f)
-            f_read = open(model_file, "r")
-            last_line = f_read.readlines()[-2]
-            f_read.close()
-            # check that common table / sql error did not occur
-            # IE: ^ pointing to position of SQL error
-            if "^" not in last_line:
+                call_command("inspectdb", table, stdout=f, stderr=err)
+                # check that errors did not occur
+            try:
+                assert err is None
                 os.remove(model_file + ".old")
                 print(
                     "Model creation from table inspection was successful - %s" % table
                 )
-            else:
+            except AssertionError as e:
                 # rollback to old file and print error notice
                 os.rename(model_file, model_file + ".old")
                 print(
                     "Table inspection failed - %s - rolled back to old model file (if available)"
-                    % table
+                    % e
                 )
     except Exception as e:
         print("Table inspection failed - %s" % str(e))
