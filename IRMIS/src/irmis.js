de80bbe6e89dfce993ede7c88b2f2e4d86e3b15c
Kevin Keller <flygeneticist@gmail.com>
2019-08-20 12:24:18 +0700

    clean up JS code

--- IRMIS/src/irmis.js
+++ IRMIS/src/irmis.js
@@ -1,8 +1,2 @@
 import './styles/irmis.scss';
 var road_api = require('./js/roads_api.js');
-
-console.log("Getting list of Roads MetaData...");
-road_api.getRoadMetadata();
-
-console.log("Getting single Road GeoData...");
-road_api.getRoadGeometry(903);
--- IRMIS/src/js/roads_api.js
+++ IRMIS/src/js/roads_api.js
@@ -1,23 +1,13 @@
-var headers = { "Content-Type": "application/json", }
+const headers = { 'Content-Type': 'application/json' };
 
 function getRoadMetadata() {
-    window.fetch('http://127.0.0.1:8000/assets/roads', { method: 'GET', headers: headers })
-    .then(function(response) {
-        console.log(response);
-        return response.json();
-    }).then(function(json) {
-        console.log(json);
-    });
+    window.fetch('http://127.0.0.1:8000/assets/roads', { method: 'GET', headers })
+        .then(response => response.json());
 }
 
 function getRoadGeometry(roadId) {
-    window.fetch('http://127.0.0.1:8000/assets/roads/' + roadId.toString(), { method: 'GET', headers: headers })
-    .then(function(response) {
-        console.log(response);
-        return response.json();
-    }).then(function(json) {
-        console.log(json);
-    });
+    window.fetch(`http://127.0.0.1:8000/assets/roads/${roadId.toString()}`, { method: 'GET', headers })
+        .then(response => response.json());
 }
 
 module.exports = { getRoadMetadata, getRoadGeometry };
