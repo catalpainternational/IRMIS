7fb855ca976b023ce2c7956b71fc41c096089a3c
Kevin Keller <flygeneticist@gmail.com>
2019-08-20 15:05:32 +0700

    async func & extra then for json

--- IRMIS/src/js/roads_api.js
+++ IRMIS/src/js/roads_api.js
@@ -1,19 +1,19 @@
 const headers = { 'Content-Type': 'application/json' };
 
-function getRoadMetadata() {
-    window.fetch(`${window.location.origin}/assets/roads`, { method: 'GET', headers })
+async function getRoadMetadata() {
+    return window.fetch(`${window.location.origin}/assets/roads`, { method: 'GET', headers })
         .then((response) => {
             if (response.ok) return response.json();
             throw new Error('Network response was not ok.');
-        });
+        }).then(json => json);
 }
 
-function getRoadGeometry(roadId) {
-    window.fetch(`${window.location.origin}/assets/roads/${roadId.toString()}`, { method: 'GET', headers })
+async function getRoadGeometry(roadId) {
+    return window.fetch(`${window.location.origin}/assets/roads/${roadId.toString()}`, { method: 'GET', headers })
         .then((response) => {
             if (response.ok) return response.json();
             throw new Error('Network response was not ok.');
-        });
+        }).then(json => json);
 }
 
 exports = { getRoadMetadata, getRoadGeometry };
