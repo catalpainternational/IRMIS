cad4f61cbdd17041c0ecfb40fa5203936d62ac7a
Kevin Keller <flygeneticist@gmail.com>
2019-08-20 14:44:08 +0700

    check fetch promise is OK before returning

--- IRMIS/src/js/roads_api.js
+++ IRMIS/src/js/roads_api.js
@@ -2,12 +2,18 @@ const headers = { 'Content-Type': 'application/json' };
 
 function getRoadMetadata() {
     window.fetch(`${window.location.origin}/assets/roads`, { method: 'GET', headers })
-        .then(response => response.json());
+        .then((response) => {
+            if (response.ok) return response.json();
+            throw new Error('Network response was not ok.');
+        });
 }
 
 function getRoadGeometry(roadId) {
     window.fetch(`${window.location.origin}/assets/roads/${roadId.toString()}`, { method: 'GET', headers })
-        .then(response => response.json());
+        .then((response) => {
+            if (response.ok) return response.json();
+            throw new Error('Network response was not ok.');
+        });
 }
 
 exports = { getRoadMetadata, getRoadGeometry };
