4c2c8c6a6a886acd257ea8aedab8a93bbe7d3120
Sérgio Esteves <sergioesteves7@gmail.com>
2019-08-23 10:26:06 +0900

    Change layout view (#85)
    * update map & table id selectors
    
    * remove empty line
    
    * add change view functionality
    
    * update CHANGELOG.md
    
    * tslint double quotes rule
    
    * remove wagtail page title display
    
    * update layout
    
    * tslint
    
    * redraw map on resize

--- IRMIS/src/side_menu.js
+++ IRMIS/src/side_menu.js
@@ -2,33 +2,46 @@ const filters = ["road-condition", "road-code"];
 
 export function collapse_side_menu() {
     const collapsedSideMenu = document.getElementById("collapsed-side-menu");
+    const mapTable = document.getElementById("map-table-irmis");
     const sideMenu = document.getElementById("side-menu");
-    const mapTable = document.getElementById("map-table");
 
     mapTable.style.maxWidth = "calc(100% - 30px)";
     mapTable.style.flex = "0 0 100%";
     sideMenu.hidden = true;
     collapsedSideMenu.classList.add("d-flex");
+    roads.map.lMap.invalidateSize();
 }
 
 export function expand_side_menu() {
     const collapsedSideMenu = document.getElementById("collapsed-side-menu");
+    const mapTable = document.getElementById("map-table-irmis");
     const sideMenu = document.getElementById("side-menu");
-    const mapTable = document.getElementById("map-table");
 
     mapTable.style.maxWidth = "75%";
     mapTable.style.flex = "0 0 75%";
     sideMenu.hidden = false;
     collapsedSideMenu.classList.remove("d-flex");
+    roads.map.lMap.invalidateSize();
 }
 
-export function change_view(element) {
+export function change_view(element, view) {
+    const mapTable = document.getElementById("map-table-irmis");
     const siblings = document.getElementById("view").children;
+
+    if (view === 0) {
+        mapTable.className = "col-9 map table";
+    } else if (view === 1) {
+        mapTable.className = "col-9 map";
+    } else {
+        mapTable.className = "col-9 table";
+    }
+
     for (let index = 0; index < siblings.length; index += 1) {
         const sibling = siblings[index];
         if (sibling !== element) { sibling.classList.remove("active"); }
     }
     element.classList.add("active");
+    roads.map.lMap.invalidateSize();
 }
 
 export function toggle_filter(element, elementId) {
