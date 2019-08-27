c121953b61570d82a672c14c27011a33ffa29fc4
Joshua Brooks <josh.vdbroek@gmail.com>
2019-08-27 17:24:36 +0900

    adds basemap app and basemap.municipality model

---
+++ IRMIS/basemap/models.py
@@ -0,0 +1,11 @@
+from django.contrib.gis.db import models
+
+class Municipality(models.Model):
+    '''
+    Formerly called 'District'. The 13 areas which
+    make up the top level of Timor's administrative area hierarchy.
+    Generated from the base data received in GDrive August 2019.
+    '''
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    geom = models.MultiPolygonField(srid=32751)
