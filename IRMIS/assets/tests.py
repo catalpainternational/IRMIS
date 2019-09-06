c43e29b1cf8a76eb8729cae5f6b990538f9ef729
Lee <md8n@md8n.com>
2019-09-06 10:48:28 +0900

    first pass at tests

--- IRMIS/assets/tests.py
+++ IRMIS/assets/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.core.files.base import ContentFile
 from .models import CollatedGeoJsonFile, Road
+import json
 import pytest
 
 
@@ -91,12 +92,52 @@ def test_geojson_details_does_not_error(client, django_user_model):
     # create a user
     user = django_user_model.objects.create_user(username="user1", password="bar")
     client.force_login(user)
-    # create a road
+    # create a GeoJSON file
     road = CollatedGeoJsonFile.objects.create(key="test", geobuf_file=ContentFile(b""))
-    # hit the road api
+    # hit the GeoJSON api
     url = reverse("geojson_details")
     response = client.get(url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_road_chunks_requires_auth(client):
+    """ This test will fail if an unauthenticated request can access the roads api """
+    url = reverse("road_chunks")
+    response = client.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_road_chunks_does_not_error(client, django_user_model):
+    """ This test will fail if the road chunks api throws an error """
+    # create a user
+    user = django_user_model.objects.create_user(username="user1", password="bar")
+    client.force_login(user)
+    # create a road
+    road = Road.objects.create()
+    # hit the road chunks api
+    url = reverse("road_chunks")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_road_chunks_reports_all_roads(client, django_user_model):
+    """ This integration test will fail if the road chunks api does not return a correct total count of roads """
+    """ This test requires access to the actual DB for it to have any real meaning """
+    # create a user
+    user = django_user_model.objects.create_user(username="user1", password="bar")
+    client.force_login(user)
+    # get an actual count of all roads
+    road_count = Road.objects.count()
+    # hit the road chunks api
+    url = reverse("road_chunks")
+    response = client.get(url)
+    chunks = json.loads(response.content)
+    # for this assertion to really work we need to extract all of the chunks
+    # sum their individual values and then finally compare that to road_count
+    assert road_count == 0
 
 
 @pytest.mark.django_db
