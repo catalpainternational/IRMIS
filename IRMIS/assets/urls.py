2c6b0b0ab5ea25f60323038920d78ba12ba92365
Lee <md8n@md8n.com>
2019-09-10 16:17:46 +0900

    blacked

--- IRMIS/assets/urls.py
+++ IRMIS/assets/urls.py
@@ -1,6 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import RoadViewSet, geojson_details, protobuf_road_set, road_chunks_set, road_update
+from .views import (
+    RoadViewSet,
+    geojson_details,
+    protobuf_road_set,
+    road_chunks_set,
+    road_update,
+)
 
 router = routers.DefaultRouter()
 router.register(r"roads", RoadViewSet, basename="road")
