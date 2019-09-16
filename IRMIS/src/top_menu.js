f4598e68013ddc0308cfe94b9e5cbc27cd5debfa
PeteCoward <peter@catalpa.io>
2019-09-16 17:33:01 +0900

    remove jquery from topmenu

--- IRMIS/src/top_menu.js
+++ IRMIS/src/top_menu.js
@@ -1,9 +1,6 @@
-import $ from "jquery";
-
-$(document).ready(function(){
-    $('#topmenu_toggle').click(() => {
+window.addEventListener('load', function() {
+    document.querySelector('#topmenu_toggle').addEventListener('click', () => {
         var dropdown = document.getElementById("dropdown-menu");
         dropdown.hidden = !dropdown.hidden;
     });
 });
-
