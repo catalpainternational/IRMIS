809e5cbc21cada05e0ec72faf4e4a958c418de5d
Lee <md8n@md8n.com>
2019-09-16 11:06:36 +0900

    Typescript generation for protoc

---
+++ IRMIS/src/assets/assets_api.ts
@@ -0,0 +1,35 @@
+import { Road, Roads } from "../../protobuf/roads_pb";
+import { ConfigAPI } from "./configAPI";
+
+/** getRoadsMetadataChunks
+ *
+ * Retrieves the details for the road metadata chunks from the server
+ *
+ * @returns a map {id: road_object}
+ */
+export function getRoadsMetadataChunks() {
+    const assetTypeUrlFragment = "road_chunks";
+    const metadataUrl = `${ConfigAPI.requestAssetUrl}/${assetTypeUrlFragment}`;
+
+    return fetch(metadataUrl, ConfigAPI.requestAssetInit())
+        .then((jsonResponse) => (jsonResponse.json()));
+}
+
+/** getRoadsMetadata
+ *
+ * Retrieves the road metadata from the server
+ *
+ * @returns a map {id: road_object}
+ */
+export function getRoadsMetadata(chunkName: string): Promise<Road[]> {
+    const assetTypeUrlFragment = "protobuf_roads";
+    chunkName = chunkName || "";
+    const metadataUrl = `${ConfigAPI.requestAssetUrl}/${assetTypeUrlFragment}/${chunkName}`;
+
+    return fetch(metadataUrl, ConfigAPI.requestAssetInit())
+        .then((metadataResponse) => (metadataResponse.arrayBuffer()))
+        .then((protobufBytes) => {
+            const uintArray = new Uint8Array(protobufBytes);
+            return Roads.deserializeBinary(uintArray).getRoadsList();
+        });
+}
