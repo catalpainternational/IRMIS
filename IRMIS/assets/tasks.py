2749ed6e4b093181de1c9a19823247d512614a06
PeteCoward <peter@catalpa.io>
2019-09-30 16:45:33 +0900

    related_object_id attributes do not exist

--- IRMIS/assets/tasks.py
+++ IRMIS/assets/tasks.py
@@ -257,8 +257,6 @@ def populate_from_csv(road, row):
     ]
     for attr, key, mapping in mapping_assignments:
         if row[key]:
-            if not hasattr(road, attr):
-                raise Exception("road has not attribute {}".format(attr))
             setattr(road, attr, mapping[row[key]])
 
     if row["Road link name"]:
