15824aa8e28072acf14f4234f4820e3d67b4b397
Lee Humphries <lee@md8n.com>
2019-11-07 14:59:17 +0900

    Clean out redundant API method

--- IRMIS/src/reportManager.js
+++ IRMIS/src/reportManager.js
@@ -1,12 +1,4 @@
-import { getRoadReports, getRoadSurveyReports } from "./assets/reportsAPI";
-
-export function getRoadSurveyReport(roadCode) {
-    return Promise.resolve(getRoadSurveyReports(roadCode))
-        .then((surveyReportList) => {
-            // document.dispatchEvent(new CustomEvent("estrada.auditTable.roadAuditDataAdded", {detail: {auditList}}));
-            return surveyReportList;
-        });
-}
+import { getRoadReports } from "./assets/reportsAPI";
 
 export function getRoadReport(filters) {
     return Promise.resolve(getRoadReports(filters))
