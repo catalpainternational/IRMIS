cec9846844a5fe7fe85d80f38a44fadc2f4ea531
Sérgio Esteves <sergioesteves7@gmail.com>
2019-11-14 16:38:59 +0900

    black migration file

--- IRMIS/assets/migrations/0020_rename_data_field.py
+++ IRMIS/assets/migrations/0020_rename_data_field.py
@@ -5,38 +5,36 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('assets', '0019_road_number_lanes'),
-    ]
+    dependencies = [("assets", "0019_road_number_lanes")]
 
     operations = [
         migrations.AlterField(
-            model_name='road',
-            name='administrative_area',
+            model_name="road",
+            name="administrative_area",
             field=models.CharField(
                 default=None,
-                help_text='Choose the municipality for the road',
+                help_text="Choose the municipality for the road",
                 max_length=50,
                 null=True,
-                verbose_name='Municipality',
+                verbose_name="Municipality",
             ),
         ),
         migrations.AlterField(
-            model_name='road',
-            name='road_type',
+            model_name="road",
+            name="road_type",
             field=models.CharField(
                 blank=True,
                 choices=[
-                    ('NAT', 'National'),
-                    ('HIGH', 'Highway'),
-                    ('MUN', 'Municipal'),
-                    ('URB', 'Urban'),
-                    ('RUR', 'Rural')
+                    ("NAT", "National"),
+                    ("HIGH", "Highway"),
+                    ("MUN", "Municipal"),
+                    ("URB", "Urban"),
+                    ("RUR", "Rural"),
                 ],
-                help_text='Choose the road class',
+                help_text="Choose the road class",
                 max_length=4,
                 null=True,
-                verbose_name='Road Class',
+                verbose_name="Road Class",
             ),
         ),
     ]
