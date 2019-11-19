8a5c32b24f9345d21cc58814422104addfb8fc49
Kevin Keller <flygeneticist@gmail.com>
2019-11-19 11:31:59 +0700

    refactor w/ defaultdict and counters

--- IRMIS/assets/reports.py
+++ IRMIS/assets/reports.py
@@ -170,7 +170,10 @@ class Report:
             self.filters["primary_attributes"] = self.primary_attributes
 
         if self.validate_chainages():
-            self.filters["report_chainage"] = [self.road_start_chainage, self.road_end_chainage]
+            self.filters["report_chainage"] = [
+                self.road_start_chainage,
+                self.road_end_chainage,
+            ]
         else:
             if self.withAttributes:
                 # Road level reports must have start & end chainages to build a report.
--- IRMIS/assets/views.py
+++ IRMIS/assets/views.py
@@ -4,7 +4,7 @@ import pytz
 import reversion
 from reversion.models import Version
 from datetime import datetime
-from collections import Counter
+from collections import Counter, defaultdict
 import pytz
 
 from django.contrib.auth import get_user_model
@@ -462,89 +462,79 @@ def protobuf_reports(request):
             {"primary_attribute": primary_attributes, "road_type": [road_type]}
         )
 
-    if len(roads) > 0:
-        surveys = {}
-
-        road_codes = list(roads.values_list("road_code").distinct())
-        road_code_index = 0
-
-        for road_code in road_codes:
-            primary_road_code = road_code[road_code_index]
-            road_chainages = roads.filter(road_code=primary_road_code).values(
-                "link_start_chainage", "link_end_chainage"
-            )
-            min_chainage = road_chainages.order_by("link_start_chainage").first()[
-                "link_start_chainage"
-            ]
-            max_chainage = road_chainages.order_by("link_end_chainage").last()[
-                "link_end_chainage"
-            ]
-
-            if len(road_codes) == 1:
-                if (
-                    chainage_start != None
-                    and chainage_start > min_chainage
-                    and chainage_start < max_chainage
-                ):
-                    min_chainage = chainage_start
-
-                if (
-                    chainage_end != None
-                    and chainage_end > min_chainage
-                    and chainage_end < max_chainage
-                ):
-                    max_chainage = chainage_end
-
-            if (min_chainage == None or max_chainage == None):
-                # Without valid chainage values nothing can be done
-                continue
-
-            # pull any Surveys that cover the roads
-            for primary_attribute in primary_attributes:
-                surveys[primary_attribute] = (
-                    Survey.objects.filter(road=primary_road_code)
-                    .exclude(chainage_start__isnull=True)
-                    .exclude(chainage_end__isnull=True)
-                    .exclude(**{"values__" + primary_attribute + "__isnull": True})
-                    .order_by(
-                        "road", "chainage_start", "chainage_end", "-date_surveyed"
-                    )
-                    .distinct("road", "chainage_start", "chainage_end")
-                )
-
-            road_report = Report(
-                surveys, len(road_codes) == 1, min_chainage, max_chainage
-            )
-
-            if len(road_codes) == 1:
-                report_protobuf = road_report.to_protobuf()
-            else:
-                # Get all the statistics etc. to be generated for each road_code
-                temp_protobuf = road_report.to_protobuf()
-
-                report_filters = json.loads(report_protobuf.filter)
-                report_lengths = json.loads(report_protobuf.lengths)
-
-                report_protobuf.filter = json.dumps(
-                    {
-                        x: report_filters.get(x, []) + road_report.filters.get(x, [])
-                        for x in set(report_filters).union(road_report.filters)
-                    }
-                )
-                temp_length = {}
-                temp_length_attributes = set(report_lengths).union(road_report.lengths)
-                for attrib in temp_length_attributes:
-                    temp_length[attrib] = {}
-                    temp_length_keys = set(report_lengths.get(attrib, "")).union(road_report.lengths.get(attrib, ""))
-                    for key in temp_length_keys:
-                        curr_length = report_lengths.get(attrib, {key: 0}).get(key, 0)
-                        new_length = road_report.lengths.get(attrib, {key: 0}).get(key, 0)
-                        temp_length[attrib][key] = curr_length + new_length
-
-                report_protobuf.lengths = json.dumps(temp_length)
-
-    else:
+    if len(roads) == 0:
         return HttpResponseNotFound()
+
+    surveys = {}
+    road_codes = list(roads.values_list("road_code").distinct())
+    road_code_index = 0
+    final_filters = defaultdict(list)
+    final_lengths = Counter()
+
+    for road_code in road_codes:
+        primary_road_code = road_code[road_code_index]
+        road_chainages = roads.filter(road_code=primary_road_code).values(
+            "link_start_chainage", "link_end_chainage"
+        )
+        min_chainage = road_chainages.order_by("link_start_chainage").first()[
+            "link_start_chainage"
+        ]
+        max_chainage = road_chainages.order_by("link_end_chainage").last()[
+            "link_end_chainage"
+        ]
+
+        if len(road_codes) == 1:
+            if (
+                chainage_start != None
+                and chainage_start > min_chainage
+                and chainage_start < max_chainage
+            ):
+                min_chainage = chainage_start
+
+            if (
+                chainage_end != None
+                and chainage_end > min_chainage
+                and chainage_end < max_chainage
+            ):
+                max_chainage = chainage_end
+
+        if (min_chainage == None or max_chainage == None):
+            # Without valid chainage values nothing can be done
+            continue
+
+        # pull any Surveys that cover the roads
+        for primary_attribute in primary_attributes:
+            surveys[primary_attribute] = (
+                Survey.objects.filter(road=primary_road_code)
+                .exclude(chainage_start__isnull=True)
+                .exclude(chainage_end__isnull=True)
+                .exclude(**{"values__" + primary_attribute + "__isnull": True})
+                .order_by("road", "chainage_start", "chainage_end", "-date_surveyed")
+                .distinct("road", "chainage_start", "chainage_end")
+            )
+
+        road_report = Report(surveys, len(road_codes) == 1, min_chainage, max_chainage)
+
+        if len(road_codes) == 1:
+            report_protobuf = road_report.to_protobuf()
+        else:
+            report_filters = json.loads(report_protobuf.filter)
+            for x in set(report_filters).union(road_report.filters):
+                final_filters[x] = list(set(
+                    final_filters[x] +
+                    report_filters.get(x, []) +
+                    road_report.filters.get(x, [])
+                ))
+
+            report_lengths = json.loads(report_protobuf.lengths)
+            final_lengths += report_lengths
+            final_lengths += road_report.lengths
+
+            print(report_filters, report_lengths)
+            print(final_filters, final_lengths)
+
+    report_protobuf.filter = json.dumps(final_filters)
+    report_protobuf.lengths = json.dumps(final_lengths)
 
     return HttpResponse(
         report_protobuf.SerializeToString(), content_type="application/octet-stream"
