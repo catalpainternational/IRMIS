942e226b46d0f6ad3d41dee179c7923466da648a
md8n <lee@md8n.com>
2019-12-06 12:14:59 +0900

    Refactor report.riot (#661)

--- IRMIS/src/reportManager.js
+++ IRMIS/src/reportManager.js
@@ -1,6 +1,247 @@
+import { toChainageFormat } from "./assets/protoBufUtilities";
 import { getRoadReports } from "./assets/reportsAPI";
 
 export function getRoadReport(filters) {
     return Promise.resolve(getRoadReports(filters))
         .then((surveyReportList) => { return surveyReportList; });
 }
+
+/** The various columns shared between the different reports */
+export const reportColumns = {
+    title: {
+        // Title needs to be set per data type
+        title: window.gettext("Title"),
+        data: "title",
+        defaultContent: "",
+        orderable: false,
+    },
+    distance: {
+        title: window.gettext("Length (km)"),
+        data: "distance",
+        defaultContent: "",
+        className: "text-right",
+        orderable: false,
+        render: (data, type) => {
+            return (type === 'display')
+                ? (data / 1000).toFixed(2)
+                : data;
+        },
+    },
+    percent: {
+        title: window.gettext("Percentages (%)"),
+        data: "percent",
+        defaultContent: "",
+        className: "text-right",
+        orderable: false,
+    },
+    chainageStart: {
+        title: window.gettext("Chainage Start"),
+        data: "chainageStart",
+        defaultContent: "",
+        className: "text-right",
+        orderable: false,
+        render: (data, type) => {
+            return (type === 'display') ? toChainageFormat(data) : data;
+        },
+    },
+    chainageEnd: {
+        title: window.gettext("Chainage End"),
+        data: "chainageEnd",
+        defaultContent: "",
+        className: "text-right",
+        orderable: false,
+        render: (data, type) => {
+            return (type === 'display') ? toChainageFormat(data) : data;
+        },
+    },
+    municipality: {
+        title: window.gettext("Municipality"),
+        data: "municipality",
+        defaultContent: "",
+        orderable: false,
+    },
+    roadClass: {
+        title: window.gettext("Road Class"),
+        data: "roadClass",
+        defaultContent: "",
+        orderable: false,
+    },
+    surfaceCondition: {
+        title: window.gettext("Surface Condition (SDI)"),
+        data: "surfaceCondition",
+        defaultContent: "",
+        orderable: false,
+    },
+    surfaceType: {
+        title: window.gettext("Surface Type"),
+        data: "surfaceType",
+        defaultContent: "",
+        orderable: false,
+    },
+    surveyDate: {
+        title: window.gettext("Survey Date"),
+        data: "dateSurveyed",
+        defaultContent: "",
+        orderable: false,
+    },
+};
+
+/** The names given to the dataTables used in the reports */
+export const reportTableIds = {
+    municipality: "report-municipality-table",
+    roadClass: "report-road-class-table",
+    surfaceType: "report-surface-type-table",
+    roadStatus: "report-road-status-table",
+    technicalClass: "report-technical-class-table",
+    surfaceCondition: "report-surface-condition-table",
+};
+
+/** The names of the stacked bars used in the reports */
+export const reportBarIds = {
+    roadStatus: "report-road-status-bar",
+    technicalClass: "report-technical-class-bar",
+    surfaceCondition: "report-surface-condition-bar",
+};
+
+/** The definitions of the different reports */
+export const reportContent = {
+    0: {
+        title: window.gettext("Road Network Length"),
+        description: window.gettext("A report on the total length of the road network. The report provides total length in kilometers and percentages according to Road Status and Technical Class. A number of filters can be selected in order to generate a length report according to the required criteria"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length report"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length information. You can use filters to generate a customized report"),
+        noReportData: window.gettext("Sorry, data for the road network is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["road_status", "technical_class"],
+        },
+        visibleFilters: {
+            municipality: true,
+            roadClass: true,
+            surfaceType: true,
+            surfaceCondition: true,
+            reportDate: true,
+        },
+        reportElements: { filters: true, totalLength: true, dataTables: [reportTableIds.roadStatus, reportTableIds.technicalClass] },
+    },
+    1: {
+        title: window.gettext("Road Network Length Breakdown"),
+        description: window.gettext("A breakdown report on the total length of the road network. The report provides information on total length in kilometers and percentages as well as Road Status and Technical Class, according to a breakdown by Municipality, Road Class and Surface Type"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length Breakdown report"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length Breakdown information. You can use filters to generate a customized report"),
+        noReportData: window.gettext("Sorry, data for the road network is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["municipality", "road_class", "surface_type"],
+        },
+        visibleFilters: {
+            municipality: true,
+            roadClass: true,
+            surfaceType: true,
+            surfaceCondition: true,
+            reportDate: true,
+        },
+        reportElements: { filters: true, totalLength: true, dataTables: [reportTableIds.municipality, reportTableIds.roadClass, reportTableIds.surfaceType] },
+    },
+    2: {
+        title: window.gettext("Surface Condition"),
+        description: window.gettext("A report on Road Surface Condition. This report provides detailed Surface Condition information per segment"),
+        noReportTitle: window.gettext("Please select a road above to view Surface Condition reports"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Surface Condition information per segment of the selected road"),
+        noReportData: window.gettext("Sorry, Surface Condition data is not available for the selected road"),
+        fixedFilter: {
+            primaryattribute: ["surface_condition"],
+        },
+        visibleFilters: {
+            roadCode: true,
+            reportDate: true,
+        },
+        reportElements: { roadCodeAndChainage: true, stackedBars: [reportBarIds.surfaceCondition], dataTables: [reportTableIds.surfaceCondition] },
+    },
+    3: {
+        title: window.gettext("IRI Roughness"),
+        description: window.gettext("A report on Road IRI data. This report provides detailed information on a Road's Roughness per segment"),
+        noReportTitle: window.gettext("Please select a road above to view IRI Roughness reports"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed IRI Roughness information per segment of the selected road"),
+        noReportData: window.gettext("Sorry, IRI Roughness data is not available for the selected road"),
+        fixedFilter: {},
+        visibleFilters: {
+            roadCode: true,
+            reportDate: true,
+        },
+        reportElements: { roadCodeAndChainage: true },
+    },
+    4: {
+        title: window.gettext("Road Network Length - National Class"),
+        description: window.gettext("A report on the total length of National Class roads. The report provides total length in kilometers and percentages according to Road Status and Technical Class for National Roads"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length report for National roads"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length information for National class roads"),
+        noReportData: window.gettext("Sorry, data for National Class roads is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["road_status", "technical_class"],
+            roadtype: "NAT",
+        },
+        visibleFilters: {
+            reportDate: true,
+        },
+        reportElements: { totalLength: true, dataTables: [reportTableIds.roadStatus, reportTableIds.technicalClass] },
+    },
+    5: {
+        title: window.gettext("Road Network Length - Municipal Class"),
+        description: window.gettext("A report on the total length of Municipal Class roads. The report provides total length in kilometers and percentages according to Road Status and Technical Class for Municipal Roads"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length report for Municipal roads"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length information for Municipal class roads"),
+        noReportData: window.gettext("Sorry, data for Municipal Class roads is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["road_status", "technical_class"],
+            roadtype: "MUN",
+        },
+        visibleFilters: {
+            reportDate: true,
+        },
+        reportElements: { totalLength: true, dataTables: [reportTableIds.roadStatus, reportTableIds.technicalClass] },
+    },
+    6: {
+        title: window.gettext("Road Network Length - Rural Class"),
+        description: window.gettext("A report on the total length of Rural Class roads. The report provides total length in kilometers and percentages according to Road Status and Technical Class for Rural Roads"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length report for Rural roads"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length information for Rural class roads"),
+        noReportData: window.gettext("Sorry, data for Rural Class roads is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["road_status", "technical_class"],
+            roadtype: "RUR",
+        },
+        visibleFilters: {
+            reportDate: true,
+        },
+        reportElements: { totalLength: true, dataTables: [reportTableIds.roadStatus, reportTableIds.technicalClass] },
+    },
+    7: {
+        title: window.gettext("Road Network Length - Highway Class"),
+        description: window.gettext("A report on the total length of Highways. The report provides total length in kilometers and percentages according to Road Status and Technical Class for Highways"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length report for Highways"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length information for Highway class roads"),
+        noReportData: window.gettext("Sorry, data for Highway Class roads is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["road_status", "technical_class"],
+            roadtype: "HIGH",
+        },
+        visibleFilters: {
+            reportDate: true,
+        },
+        reportElements: { totalLength: true, dataTables: [reportTableIds.roadStatus, reportTableIds.technicalClass] },
+    },
+    8: {
+        title: window.gettext("Road Network Length - Urban Class"),
+        description: window.gettext("A report on the total length of Urban Class roads. The report provides total length in kilometers and percentages according to Road Status and Technical Class for Urban Roads"),
+        noReportTitle: window.gettext("Click on Create Report button to access the Road Network Length report for Urban roads"),
+        noReportDescription: window.gettext("The report will be shown in this area and will provide you with detailed Road Network Length information for Urban class roads"),
+        noReportData: window.gettext("Sorry, data for Urban Class roads is not available yet"),
+        fixedFilter: {
+            primaryattribute: ["road_status", "technical_class"],
+            roadtype: "URB",
+        },
+        visibleFilters: {
+            reportDate: true,
+        },
+        reportElements: { totalLength: true, dataTables: [reportTableIds.roadStatus, reportTableIds.technicalClass] },
+    },
+};
