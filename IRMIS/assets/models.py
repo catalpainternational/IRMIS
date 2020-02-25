bcb225b454a29142549f571192a1999fcbe1b24b
Joshua Brooks <josh.vdbroek@gmail.com>
2020-02-25 11:34:15 +0900

    add a GIST index to roads

--- IRMIS/assets/models.py
+++ IRMIS/assets/models.py
@@ -645,15 +645,8 @@ class Road(models.Model):
     def __str__(self,):
         return "%s(%s) %s" % (self.road_code, self.link_code, self.road_name)
 
-
-class RoadFeatureAttributes(models.Model):
-    """
-    Original data fields of the Road model shapefiles
-    """
-
-    road = models.OneToOneField("Road", on_delete=models.CASCADE)
-    attributes = JSONField()
-
+    class Meta:
+        indexes = [GistIndex(fields=("geom",))]
 
 class CollatedGeoJsonFile(models.Model):
     """ FeatureCollection GeoJson(srid=4326) files made up of collated geometries """
