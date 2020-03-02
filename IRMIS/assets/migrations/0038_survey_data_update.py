b58ebc138b83349bb6292163aed920d67f8b51d2
Lee Humphries <lee@md8n.com>
2020-03-02 10:33:36 +0900

    Move along the migrations - again

--- IRMIS/assets/migrations/0038_survey_data_update.py
+++ IRMIS/assets/migrations/0038_survey_data_update.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("assets", "0035_survey_add_asset_code"),
+        ("assets", "0037_survey_data_update"),
     ]
 
     operations = [
