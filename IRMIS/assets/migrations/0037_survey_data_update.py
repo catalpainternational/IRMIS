2fce8dbb76ddf8fbd8840cf4bd009fa53f03864e
Lee Humphries <lee@md8n.com>
2020-03-02 10:37:29 +0900

    Maybe this time I'll get the file sequence right

--- IRMIS/assets/migrations/0037_survey_data_update.py
+++ IRMIS/assets/migrations/0037_survey_data_update.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("assets", "0037_survey_data_update"),
+        ("assets", "0036_survey_add_asset_code"),
     ]
 
     operations = [
