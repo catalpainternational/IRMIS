eca43cd38b4e713ecef880a047d9b136ffe25a69
Joshua Brooks <josh.vdbroek@gmail.com>
2020-03-12 17:31:53 +0900

    Just the 'estradaroads' model

--- IRMIS/topology/models.py
+++ IRMIS/topology/models.py
@@ -5,68 +5,6 @@ from django.contrib.gis.db.models import Transform
 from assets import models as assets
 
 
-class RoadCorrectionSegment(models.Model):
-    """
-    Second-generation fixes
-    Contains the diffs only for the geometries
-    """
-
-    road_code = models.TextField(null=True, blank=True)
-    road = models.ForeignKey(
-        "assets.Road",
-        null=True,
-        blank=True,
-        help_text="A reference to a particular Assets road ID which the patch applies to",
-        on_delete=models.CASCADE,
-    )
-    patch = models.PolygonField(srid=32751, dim=2, blank=True, null=True)
-    geom = models.LineStringField(srid=32751, dim=2, blank=True, null=True)
-
-
-class InputRoad(models.Model):
-    """
-    Whitelisted and blacklisted roads
-    """
-
-    road = models.OneToOneField(assets.Road, on_delete=models.CASCADE, primary_key=True)
-    blacklist = models.BooleanField(
-        default=False,
-        help_text="True if this is a Blacklisted, never-include-road. False if this is a road to include in Topology creation.",
-    )
-    road_code = models.TextField(
-        blank=True,
-        null=True,
-        help_text="Override the road_code from the assets table where required",
-    )
-    disabled = models.BooleanField(
-        default=False,
-        help_text="True if this code is not to be included now. Like blacklist but temporary.",
-    )
-
-
-class Intersection(models.Model):
-    """
-    Mark out "valid" and "invalid" intersections for topology fixes
-    """
-
-    road_id_a = models.IntegerField()
-    road_id_b = models.IntegerField()
-    intersection = models.PointField(srid=32751, dim=2)
-
-
-class TopoRoad(models.Model):
-    """
-    Topologic model of the road
-    This is an intermediate field with a TopoGeometry field
-    """
-
-    road_code = models.TextField(primary_key=True)
-    topo_geom = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-
-
 class EstradaRoad(models.Model):
     """
     Generated, topographically correct, roads
@@ -75,14 +13,3 @@ class EstradaRoad(models.Model):
 
     road_code = models.TextField(primary_key=True)
     geom = models.LineStringField(srid=32751, dim=2, blank=True, null=True)
-
-
-class RoadAlternateCode(models.Model):
-    """
-    Where a road might have multiple codes, copy the geometry features in the singleparts
-    intermediary
-    """
-
-    src_road_code = models.TextField()
-    dest_road_code = models.TextField()
-    part = models.PolygonField(srid=32751, dim=2, blank=True, null=True)
