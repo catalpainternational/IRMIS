0025aa4777480ef2e5ca2d04a2cc2a9884581870
Sérgio Esteves <sergioesteves7@gmail.com>
2020-04-07 15:03:19 +0900

    save original project status

--- IRMIS/src/contracts/form.js
+++ IRMIS/src/contracts/form.js
@@ -1,10 +1,17 @@
 window.addEventListener("load", () => {
+    const status = document.getElementById("id_status");
     const startDate = document.getElementById("id_start_date");
     const endDate = document.getElementById("id_end_date");
     const announcementStartDate = document.getElementById("id_announcement_date");
     const submissionStartDate = document.getElementById("id_submission_date");
     const evaluationStartDate = document.getElementById("id_evaluation_date");
     const amendmentStartDate = document.getElementById("id_amendment_start_date");
+
+    let originalForm = {};
+
+    if (status) {
+        originalForm["status"] = status.value;
+    }
 
     if (startDate) {
         startDate.type = "date";
