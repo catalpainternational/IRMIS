4c906ef8f0b41009174993ac15538174052dbf2c
Sérgio Esteves <sergioesteves7@gmail.com>
2020-04-07 18:52:47 +0900

    save contract details values for comparison

--- IRMIS/src/contracts/form.js
+++ IRMIS/src/contracts/form.js
@@ -32,9 +32,19 @@ window.addEventListener("load", () => {
     const representativePhone = document.getElementById("id_rep_phone");
     const representativeEmail = document.getElementById("id_rep_email");
     const womanLed = document.getElementById("id_woman_led");
-
+    const tender = document.getElementById("id_tender");
+    const contractCode = document.getElementById("id_contract_code");
+    const contractor = document.getElementById("id_contractor");
+    const subcontractor = document.getElementById("id_subcontractor");
+    const supervisorName = document.getElementsByClassName("supervisor-name");
+    const supervisorPhone = document.getElementsByClassName("supervisor-phone");
     const endDate = document.getElementById("id_end_date");
+    const dlp = document.getElementById("id_defect_liability_days");
+    const amendmentValue = document.getElementsByClassName("amendment-value");
+    const amendmentYear = document.getElementsByClassName("amendment-year");
     const amendmentStartDate = document.getElementById("id_amendment_start_date");
+    const amendmentDuration = document.getElementById("id_amendment_duration");
+    const amendmentDescription = document.getElementById("id_amendment_description");
 
     let originalForm = {};
     let currentForm = {};
@@ -74,6 +84,19 @@ window.addEventListener("load", () => {
             saveValue(currentForm, representativePhone);
             saveValue(currentForm, representativeEmail);
             saveValue(currentForm, womanLed);
+            saveValue(currentForm, tender);
+            saveValue(currentForm, contractCode);
+            saveValue(currentForm, contractor);
+            saveValue(currentForm, subcontractor);
+            saveFormsetValue(currentForm, supervisorName);
+            saveFormsetValue(currentForm, supervisorPhone);
+            saveValue(currentForm, endDate);
+            saveValue(currentForm, dlp);
+            saveFormsetValue(currentForm, amendmentValue);
+            saveFormsetValue(currentForm, amendmentYear);
+            saveValue(currentForm, amendmentStartDate);
+            saveValue(currentForm, amendmentDuration);
+            saveValue(currentForm, amendmentDescription);
 
             if (JSON.stringify(originalForm) !== JSON.stringify(currentForm)) {
                 document.dispatchEvent(new CustomEvent("confirm-changes", { detail: { backUrl: backUrl } }));
@@ -116,6 +139,19 @@ window.addEventListener("load", () => {
     saveValue(originalForm, representativePhone);
     saveValue(originalForm, representativeEmail);
     saveValue(originalForm, womanLed);
+    saveValue(originalForm, tender);
+    saveValue(originalForm, contractCode);
+    saveValue(originalForm, contractor);
+    saveValue(originalForm, subcontractor);
+    saveFormsetValue(originalForm, supervisorName);
+    saveFormsetValue(originalForm, supervisorPhone);
+    saveValue(originalForm, endDate);
+    saveValue(originalForm, dlp);
+    saveFormsetValue(originalForm, amendmentValue);
+    saveFormsetValue(originalForm, amendmentYear);
+    saveValue(originalForm, amendmentStartDate);
+    saveValue(originalForm, amendmentDuration);
+    saveValue(originalForm, amendmentDescription);
 
     if (startDate) {
         startDate.type = "date";
