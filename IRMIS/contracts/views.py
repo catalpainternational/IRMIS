c4d32c40f46c2b9b3b302e3a66659e3c623400b3
Sérgio Esteves <sergioesteves7@gmail.com>
2020-04-20 14:42:10 +0900

    add type of work list to the project list view context

--- IRMIS/contracts/views.py
+++ IRMIS/contracts/views.py
@@ -114,6 +114,7 @@ class ProjectListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context["status_list"] = models.ProjectStatus.objects.all()
+        context["type_of_work_list"] = models.TypeOfWork.objects.all()
         return context
 
 
