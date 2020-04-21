62ff55a43982078e2b9cade8931a4af58d89ab10
Sérgio Esteves <sergioesteves7@gmail.com>
2020-04-21 15:37:56 +0900

    prepare contract filters select2

--- IRMIS/src/contracts/select2.js
+++ IRMIS/src/contracts/select2.js
@@ -11,6 +11,21 @@ window.addEventListener("load", () => {
     $("#id_projects").on('select2:unselect', toggleMultipleSelect2);
 });
 
+document.addEventListener("prepare-select2", (data) => {
+    const dataTable = data.detail.dataTable;
+    const select2Element = `#${data.detail.id}`;
+    const placeHolder = data.detail.placeHolder;
+
+    $(select2Element).select2({
+        width: "225px",
+        containerCssClass: "contracts-select2",
+        dropdownCssClass: "contracts-dropdown-select2",
+        placeholder: placeHolder,
+    });
+    $(select2Element).on('select2:select', (e) => searchDataTable(dataTable, e));
+    $(select2Element).on('select2:unselect', (e) => searchDataTable(dataTable, e));
+});
+
 function toggleMultipleSelect2(e) {
     const selectElement = e.currentTarget;
     const inputElement = selectElement.nextSibling.children.item(0).firstElementChild;
@@ -21,3 +36,16 @@ function toggleMultipleSelect2(e) {
         inputElement.classList.remove("active");
     }
 };
+
+function searchDataTable(dataTable, e) {
+    const selectElement = e.currentTarget;
+    const inputElement = selectElement.nextSibling.children.item(0).firstElementChild;
+
+    if (selectElement.value !== "") {
+        inputElement.classList.add("active");
+    } else {
+        inputElement.classList.remove("active");
+    }
+
+    dataTable.draw();
+}
