885496e5de6d15d7770903239ce058f29d95724a
Lee Humphries <lee@md8n.com>
2020-04-22 10:17:51 +0900

    removed redundant reference

--- IRMIS/contracts/forms.py
+++ IRMIS/contracts/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ModelChoiceField, ModelMultipleChoiceField, Select
+from django.forms import ModelForm, ModelMultipleChoiceField, Select
 from django.forms import modelformset_factory, inlineformset_factory
 from django.utils.translation import ugettext_lazy as _
 
