7e8c196d255b11e404aa66e498b131eb619bd21d
Sérgio Esteves <sergioesteves7@gmail.com>
2020-04-28 15:03:59 +0900

    DRY'ed up some code

--- IRMIS/src/contracts/table.js
+++ IRMIS/src/contracts/table.js
@@ -68,14 +68,7 @@ function initializeProjectsListTable(table) {
     setupSelectFilter(dataTable, "project_type_of_work_select2", window.gettext("Type of work"), textFilter, 4);
 
     // Budget value range filter
-    const valueSelectId = "value_select";
-    document.getElementById(valueSelectId).addEventListener("change", () => dataTable.draw());
-    $.fn.dataTableExt.afnFiltering.push(
-        function (oSettings, _aData, iDataIndex) {
-            let row = oSettings.aoData[iDataIndex]._aData;
-            return valueRangeFilter(row, 5, valueSelectId);
-        }
-    );
+    setupValueRangeFilter(dataTable, 5);
 }
 
 function initializeTendersListTable(table) {
@@ -110,14 +103,7 @@ function initializeTendersListTable(table) {
     setupSelectFilter(dataTable, "tender_type_of_work_select2", window.gettext("Type of work"), multipleTextFilter, 6);
 
     // Budget value range filter
-    const valueSelectId = "value_select";
-    document.getElementById(valueSelectId).addEventListener("change", () => dataTable.draw());
-    $.fn.dataTableExt.afnFiltering.push(
-        function (oSettings, _aData, iDataIndex) {
-            let row = oSettings.aoData[iDataIndex]._aData;
-            return valueRangeFilter(row, 7, valueSelectId);
-        }
-    );
+    setupValueRangeFilter(dataTable, 7);
 }
 
 function initializeContractsListTable(table) {
@@ -152,14 +138,7 @@ function initializeContractsListTable(table) {
     setupSelectFilter(dataTable, "contract_status_select2", window.gettext("Contract status"), textFilter, 5);
 
     // Budget value range filter
-    const valueSelectId = "value_select";
-    document.getElementById(valueSelectId).addEventListener("change", () => dataTable.draw());
-    $.fn.dataTableExt.afnFiltering.push(
-        function (oSettings, _aData, iDataIndex) {
-            let row = oSettings.aoData[iDataIndex]._aData;
-            return valueRangeFilter(row, 6, valueSelectId);
-        }
-    );
+    setupValueRangeFilter(dataTable, 6);
 }
 
 function initializeCompaniesListTable(table) {
@@ -216,6 +195,17 @@ function setupSelectFilter(dataTable, selectId, placeHolder, filterFunc, columnN
         function (oSettings, _aData, iDataIndex) {
             let row = oSettings.aoData[iDataIndex]._aData;
             return filterFunc(row, columnNo, selectId);
+        }
+    );
+}
+
+function setupValueRangeFilter(dataTable, columnNo) {
+    const valueSelectId = "value_select";
+    document.getElementById(valueSelectId).addEventListener("change", () => dataTable.draw());
+    $.fn.dataTableExt.afnFiltering.push(
+        function (oSettings, _aData, iDataIndex) {
+            let row = oSettings.aoData[iDataIndex]._aData;
+            return valueRangeFilter(row, columnNo, valueSelectId);
         }
     );
 }
